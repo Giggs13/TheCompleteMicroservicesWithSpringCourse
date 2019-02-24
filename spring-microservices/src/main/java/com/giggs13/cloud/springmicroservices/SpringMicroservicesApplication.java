f4bbaf011ed976e4143366f836d04313264ea5fd
Giggs13 <Andrej.Malykhin.13@gmail.com>
2019-02-24 22:23:10 +0200

    5. Getting Started: Spring Boot - External Configuration Approaches

--- spring-microservices/src/main/java/com/giggs13/cloud/springmicroservices/SpringMicroservicesApplication.java
+++ spring-microservices/src/main/java/com/giggs13/cloud/springmicroservices/SpringMicroservicesApplication.java
@@ -2,12 +2,19 @@ package com.giggs13.cloud.springmicroservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringMicroservicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMicroservicesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroservicesApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
 }
