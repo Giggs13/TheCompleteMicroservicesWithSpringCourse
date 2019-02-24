ab1d32615222d0b5399d2d92e7b70b0ddf4c80d2
Giggs13 <Andrej.Malykhin.13@gmail.com>
2019-02-24 23:23:57 +0200

    6. Getting Started: Spring Boot - Importing Bean Configurations

--- spring-microservices/src/main/java/com/giggs13/cloud/springmicroservices/SpringMicroservicesApplication.java
+++ spring-microservices/src/main/java/com/giggs13/cloud/springmicroservices/SpringMicroservicesApplication.java
@@ -2,19 +2,13 @@ package com.giggs13.cloud.springmicroservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@RestController
+@ImportResource("/simple-context.xml")
 public class SpringMicroservicesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMicroservicesApplication.class, args);
-    }
-
-    @GetMapping("/")
-    public String helloWorld() {
-        return "Hello World!";
     }
 }
---
+++ spring-microservices/src/main/java/com/giggs13/cloud/springmicroservices/configuration/DemoConfiguration.java
@@ -0,0 +1,16 @@
+package com.giggs13.cloud.springmicroservices.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class DemoConfiguration {
+
+    @Bean
+    public List<String> cats() {
+        return Arrays.asList("Bengal", "Lion");
+    }
+}
